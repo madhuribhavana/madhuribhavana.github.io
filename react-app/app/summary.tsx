2a2b835944d3ade7529225658714327dfa8d0848
dao-heart <venkatarishithrayal.dande@sjsu.edu>
2026-06-28 23:13:39 -0700

    #react-conversion - Add react essential files.

---
+++ react-app/app/summary.tsx
@@ -0,0 +1,14 @@
+export const Summary = () => {
+    return (
+        <div id="summary-block" className="row d-flex align-items-center flex-column">
+            <div className="main-header row d-flex justify-content-center">
+          <h5>UX RESEARCH @ GOOGLE</h5>
+        </div>
+        <div className="sub-header row d-flex justify-content-center">
+          <h1 className="col-7">
+            Helping ad publishers and developers monetize their apps
+          </h1>
+        </div>
+        </div>
+    );
+}
